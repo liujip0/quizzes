eb27ee6c1ea0f8db450c506dbbd6ae79d9cf7ba4
liujip0 <liujip2020@gmail.com>
2026-06-24 22:51:31 +0800

    verify token in context

--- worker/auth/tokens.ts
+++ worker/auth/tokens.ts
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken";
 
+export type TokenPayload = { user: string; userSecret: string };
+
 export function generateToken(
   username: string,
   userSecret: string,
   jwtSecret: string,
 ) {
-  return jwt.sign({ user: username, userSecret }, jwtSecret, {
+  return jwt.sign({ user: username, userSecret } as TokenPayload, jwtSecret, {
     expiresIn: "7d",
   });
 }
--- worker/context.ts
+++ worker/context.ts
@@ -1,11 +1,30 @@
 import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
+import type { User } from "../types/Users.ts";
 import "../worker-configuration.d.ts";
+import type { TokenPayload } from "./auth/tokens.ts";
 
-export function createContext(
+export async function createContext(
   { req, resHeaders }: FetchCreateContextFnOptions,
   env: Env,
 ) {
-  return { req, resHeaders, env };
+  const token = req.headers.get("Authorization")?.split(" ")[1] ?? "";
+
+  try {
+    const payload = jwt.verify(token, env.JWT_SECRET) as TokenPayload;
+
+    const user = await env.DB.prepare("SELECT * FROM Users WHERE username = ?")
+      .bind(payload.user)
+      .first<User>();
+
+    if (!user || user.secret !== payload.userSecret) {
+      throw new JsonWebTokenError("Token does not match database record");
+    }
+
+    return { req, resHeaders, env, user: payload.user };
+  } catch (error: JsonWebTokenError | unknown) {
+    return { req, resHeaders, env, user: null };
+  }
 }
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
