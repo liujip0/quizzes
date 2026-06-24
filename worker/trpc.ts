c6be0a7e619fb2f53970b1264f98e5e855453dce
liujip0 <liujip2020@gmail.com>
2026-06-24 22:55:15 +0800

    add authenticatedProcedure

--- worker/trpc.ts
+++ worker/trpc.ts
@@ -1,4 +1,4 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import type { Context } from "./context.ts";
 
 /**
@@ -13,3 +13,13 @@ const t = initTRPC.context<Context>().create();
  */
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const authenticatedProcedure = t.procedure.use(async ({ ctx, next }) => {
+  if (!ctx.user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You must be logged in to access this resource",
+    });
+  }
+
+  return next({ ctx });
+});
